2d7fdc9aaedda60fd436018cfac818f7777ebbf2
Fatih Deniz <deniz.fatih@hotmail.de>
2022-05-31 22:21:25 +0200

    Generic Repository Pattern

---
+++ src/Api/Core/BlazorDictionary.Api.Application/Interfaces/Repositories/IEntryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BlazorDictionary.Api.Domain.Models;
+
+namespace BlazorDictionary.Api.Application.Interfaces.Repositories
+{
+    public interface IEntryRepository : IGenericRepository<Entry>
+    {
+       
+    }
+}
---
+++ src/Api/Core/BlazorDictionary.Api.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlazorDictionary.Api.Domain.Models;
+
+namespace BlazorDictionary.Api.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where  TEntity : BaseEntity
+    {
+        Task<int> AddAsync(TEntity entity);
+
+        Task<int> UpdateAsync(TEntity entity);
+
+        Task<int> DeleteAsync(Guid id);
+    }
+}
