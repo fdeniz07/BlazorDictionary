4201994dedd0c109a1954630769aaf399bfbc17f
Fatih Deniz <deniz.fatih@hotmail.de>
2022-06-06 23:00:52 +0200

    Service Registrations

--- src/Api/Core/BlazorDictionary.Api.Application/Extensions/Registration.cs
+++ src/Api/Core/BlazorDictionary.Api.Application/Extensions/Registration.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using FluentValidation;
+﻿using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace BlazorDictionary.Api.Application.Extensions
 {
